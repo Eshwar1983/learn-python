9a7284f7ed8942db9db853422dcb787e3afddc3d
Eshwar-gowda <30590284+Eshwar1983@users.noreply.github.com>
2026-08-03 16:37:03 +0530

    modified
    Refactor API endpoints to use async functions and MongoDB connection.

--- app.py
+++ app.py
@@ -13,42 +13,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.route('/api/data', methods=['GET'])
-def get_data():
-  try:
-    documents = []
-    # Fetch all documents from the collection
-    for doc in collection.find():
-      # Convert ObjectId to string for JSON compatibility
-      doc['_id'] = str(doc['_id'])
-      documents.append(doc)
-    return jsonify(documents)
-  except Exception as e:
-    return jsonify({"error": str(e)}), 500
+MONGO_URI = os.getenv("MONGO_URI")
+client = AsyncIOMotorClient(MONGO_URI)
+db = client.get_database("testdb")
 
-@app.route('/api/data', methods=['POST'])
-def post_item():
-  form_data = request.json
-  if not form_data:
-    return jsonify({'error': 'No data provided'}), 400
-  collection.insert_one(form_data)
-  return jsonify({'message': 'Data pushed successfully!'}), 201
 
-@app.route("/api/data/<string:item_id>", methods=['PUT'])
-def update_item(item_id):
-  try:
-    form_data = request.json
-    print(f"Updated data {form_data}")
-    result = collection.update_one({"_id": ObjectId(item_id)}, {"$set": form_data})
-    if result.matched_count == 0:
-      return jsonify({"error": "Item not found"}), 404
-    return jsonify({"message": "Item updated successfully", "updated_filds": form_data}), 200
-  except Exception as e:
-     return jsonify({"error": str(e)})
-
-@app.route("/api/data/<id>", methods=["DELETE"])
-def delete_item(id):
-  result = collection.delete_one({"_id": ObjectId(id)})
-  if result.deleted_count == 1:
-    return jsonify({"success": True, "message": "Deleted successfully"}), 200
-  return jsonify({"duccess": False, "message": "Item not found"}), 400
+@app.get("/api/data")
+async def get_data():
+  items = await db.items.find().to_list(100)
+  for item in items:
+    item["_id"] = str(item["_id"])
+  return {"message": "Hello from Python!", "items": items}
